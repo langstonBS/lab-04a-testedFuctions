daedded55cbbea562a05e56bd8a764bbebdf8cbc
langstonBS <langston.beckwithstanley@gmail.com>
2020-03-26 10:57:33 -0700

    starting folder

---
+++ test/tests.js
@@ -0,0 +1,13 @@
+const test = QUnit.test;
+
+test('time to test a function', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(true, false);
+});
